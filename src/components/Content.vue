2948b116b7eedfa42c0ca6f0b63b5d2a8b624803
Nadia Kaplia <kapelkanadia@gmail.com>
2019-12-20 13:49:42 +0200

    lesson 6

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -1,34 +1,5 @@
 <template>
     <router-view/>
-
-<!--            #card-activity.card(style='display: none;')-->
-<!--                .day TODAY-->
-<!--                .activity-tasks(v-for='taskActivity in activity' :key='taskActivity.name')-->
-<!--                    .box.box-1-->
-<!--                         h1  {{taskActivity.task}}-->
-<!--                         // <h2></h2>-->
-<!--                         // <h3></h3>-->
-<!--                    .box.box-2-->
-<!--                         p  {{taskActivity.time}}-->
-<!--                .task-->
-<!--                     p-->
-<!--                         | During a project build, it is necessary to-->
-<!--                         | evaluate the product design and development-->
-<!--                         | against project requirements and outcomes-->
-<!--                p.image-->
-<!--                     img(src='@/assets/image-1.jpg' alt='Фотография 1' width='100' height='100' v-on:click='imageClick(0)')-->
-<!--                     img(src='@/assets/image-2.jpg' alt='Фотография 2' width='100' height='100' v-on:click='imageClick(1)')-->
-<!--                     img(src='@/assets/image-3.jpg' alt='Фотография 3' width='100' height='100' v-on:click='imageClick(2)')-->
-<!--                     img(src='@/assets/image-4.jpg' alt='Фотография 4' width='100' height='100' v-on:click='imageClick(3)')-->
-<!--            #card-tasks.card(style='display: none;')-->
-<!--                .activity-tasks(v-for='task in taskList' :key='task.name')-->
-<!--                    .box.task-name-->
-<!--                         | {{task.name}}-->
-<!--                    .box.task-description-->
-<!--                         | {{task.description}}-->
-<!--                    .box.task-deadline-->
-<!--                         | {{task.deadline}}-->
-
 </template>
 
 <script lang="ts">
